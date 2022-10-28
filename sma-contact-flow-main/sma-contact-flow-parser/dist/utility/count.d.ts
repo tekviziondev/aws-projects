01c037c4572a2d9df4e3f3196049c5adfd9ebe6b
Karthikeyan <88785130+Karthikeyan021298@users.noreply.github.com>
2022-10-28 17:42:34 +0530

    Revert "Revert "Revert "Code-Cleaning(1)"""
    This reverts commit b3956b594d1f4885e0c53362a226c1125deb0f4b.

--- sma-contact-flow-main/sma-contact-flow-parser/dist/utility/count.d.ts
+++
@@ -1,7 +0,0 @@
-/**
-  * This function will count the number of occurences of the of string in the Text
-  * @param str
-  * @param find
-  * @returns count
-  */
-export declare function count(str: any, find: any): number;
