8ac2bb9323c0652384467e13416d44ea6efe2b45
Karthikeyan <88785130+Karthikeyan021298@users.noreply.github.com>
2022-11-03 13:05:11 +0530

    removing dist folder

--- sma-contact-flow-main/sma-contact-flow-parser/dist/contact-flow-loader.d.ts
+++
@@ -1,1 +0,0 @@
-export declare function loadContactFlow(amazonConnectInstanceID: string, amazonConnectContactFlowID: string, bucket: string, smaEvent: any, type: string): Promise<any>;
--- sma-contact-flow-main/sma-contact-flow-parser/dist/utility/ErrorTypes.d.ts
+++
@@ -1,9 +0,0 @@
-export declare enum ErrorTypes {
-    InputTimeLimitExceeded = "InputTimeLimitExceeded",
-    InvalidDigitsReceived = "InvalidDigitsReceived",
-    NoMatchingCondition = "NoMatchingCondition",
-    ConnectionTimeLimitExceeded = "ConnectionTimeLimitExceeded",
-    CallFailed = "CallFailed",
-    InvalidPhoneNumber = "InvalidPhoneNumber",
-    NoMatchingError = "NoMatchingError"
-}
