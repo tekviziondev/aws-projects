0994840756d68205b7ad9b368268f263229b107d
Karthikeyan <88785130+Karthikeyan021298@users.noreply.github.com>
2023-01-03 14:01:46 +0530

    Cdk changes 2

---
+++ sma-contact-flow/chime-sma-translator-cdk/resource/env-variables-intialization.d.ts
@@ -0,0 +1,11 @@
+declare type Data = {
+    REGION: any;
+    DESTINATION_LOCATION: any;
+    FAILURE_SPEECH_SSML: any;
+    FAILURE_AUDIO_FILE_LOCATION: any;
+    BUCKET_NAME: any;
+    CONNECT_INSTANCE_ID: any;
+    CONTACT_FLOW_ID: any;
+};
+export declare const Attributes: Data;
+export {};
